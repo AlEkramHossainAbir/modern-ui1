1aaa2525c0252a597e09ed3ab9fadba25ace805b
Your Name <alabir65@gmail.com>
2022-04-18 12:35:13 +0600

    Header complete

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
+import './header.css'
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is Header</h1>
+        <div className="gpt3__header section__padding" id="home">
+            <div className="gpt3__header-content">
+                <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                    Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                    unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    It has survived not only five centuries, but also the leap into electronic typesetting,
+                    remaining essentially unchanged. It was popularised in the 1960s with the release
+                    of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                    publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+                <div className="gpt3__header-content__input">
+                    <input type="email" placeholder="Your Email Address" />
+                    <button type="button">Get Started</button>
+                </div>
+
+                <div className="gpt3__header-content__people">
+                    <img src={people} alt="people"/>
+
+                    <p>1,600 people requested access a visit in last 24 hours</p>
+                </div>
+            </div>
+            <div className="gpt3__header-image">
+                <img src={ai} alt="ai"/>
+            </div>
         </div>
     );
 };
